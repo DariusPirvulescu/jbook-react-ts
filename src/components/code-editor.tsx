72642b206be20f81c8c209f0feb2bfa09fdf123f
DariusPirvulescu <organicdarius@gmail.com>
2021-06-17 20:52:19 +0300

    Add a format button, format with prettier, change theme to dark

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,7 @@
+import { useRef } from 'react'
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import prettier from 'prettier'
+import parser from 'prettier/parser-babel'
 
 interface CodeEditorProps {
   initialValue: string
@@ -6,33 +9,49 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>()
 
   const editorDidMount: EditorDidMount = (getValue, editor) => {
+    console.log('editor', editor)
+    editorRef.current = editor
     editor.onDidChangeModelContent(() => {
       onChange(getValue())
-      console.log('newv', getValue())
     })
 
     editor.getModel()?.updateOptions({ tabSize: 2 })
   }
 
+  const onClickFormat = () => {
+    const unformatted = editorRef.current.getModel().getValue()
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser]
+    })
+    console.log(formatted)
+  editorRef.current.setValue(formatted)
+  }
+
   return (
-    <MonacoEditor
-      value={initialValue}
-      editorDidMount={editorDidMount}
-      height="300px"
-      language="javascript"
-      options={{ 
-        wordWrap: "on", 
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <>
+      <button onClick={onClickFormat}>Format</button>
+      <MonacoEditor
+        value={initialValue}
+        editorDidMount={editorDidMount}
+        height="300px"
+        language="javascript"
+        theme="dark"
+        options={{ 
+          wordWrap: "on", 
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </>
   );
 };
 
