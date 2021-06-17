0e1d17a1e1cd0096fb210e81326f518e60d58f96
DariusPirvulescu <organicdarius@gmail.com>
2021-06-17 19:14:43 +0300

    Add initialValue prop to CodeEditor

--- src/index.tsx
+++ src/index.tsx
@@ -63,7 +63,7 @@ const App = () => {
 
   return (
     <div>
-      <CodeEditor />
+      <CodeEditor initialValue="const a = 30" />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
