4bc2a0c14a16929a885e92fb67e58d7103ad5998
DariusPirvulescu <organicdarius@gmail.com>
2021-06-27 22:05:20 +0300

    Fix bug (clicking inside the editor will close it)

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,34 +1,41 @@
-import { useState, useEffect } from 'react'
-import MDEditor from '@uiw/react-md-editor'
-
+import { useState, useEffect, useRef } from "react";
+import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
-  const [editing, setEditing] = useState(false)
+  const [editing, setEditing] = useState(false);
+  const previewRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const listener = () => {
-      setEditing(false)
-    }
+    const listener = (event: MouseEvent) => {
+      if (
+        previewRef.current &&
+        event.target &&
+        previewRef.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
 
-    document.addEventListener('click', listener, { capture: true })
+    document.addEventListener("click", listener, { capture: true });
 
-    return () => document.removeEventListener('click', listener, { capture: true })
-
-  }, [])
+    return () =>
+      document.removeEventListener("click", listener, { capture: true });
+  }, []);
 
   if (editing) {
     return (
-      <div>
+      <div ref={previewRef}>
         <MDEditor />
       </div>
-    )
+    );
   }
 
   return (
     <div onClick={() => setEditing(true)}>
       <MDEditor.Markdown source="# Henlo" />
     </div>
-  )
-}
+  );
+};
 
-export default TextEditor
+export default TextEditor;
