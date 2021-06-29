7133b6084210e1c87f54ff3596e94f04b2342c2d
DariusPirvulescu <organicdarius@gmail.com>
2021-06-29 23:20:53 +0300

    Replace local state with redux in CodeCell & TextEditor

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -3,29 +3,36 @@ import CodeEditor from "./code-editor"
 import Preview from "./preview";
 import bundler from '../bundler'
 import Resizable from './resizable'
+import { Cell } from "../state/cell";
+import { useActions } from "../hooks/use-actions";
 
-const CodeCell = () => {
+interface CodeCellProps {
+  cell: Cell
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState("")
   const [error, setError] = useState("")
-  const [input, setInput] = useState("");
+
+  const { updateCell } = useActions()
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundler(input);
+      const output = await bundler(cell.content);
       setCode(output.code);
       setError(output.err)
     }, 750);
 
     return () => clearTimeout(timer);
-  }, [input]);
+  }, [cell.content]);
   
   return  (
     <Resizable direction="vertical">
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction="horizontal">
           <CodeEditor
-            initialValue="const a = 1;"
-            onChange={(value) => setInput(value)}
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
         <Preview code={code} err={error} />
--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,12 +1,19 @@
 import './text-editor.css'
 import { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
+import { Cell } from '../state/cell';
+import { useActions } from '../hooks/use-actions';
 
+interface TextEditorProps {
+  cell: Cell
+}
 
-const TextEditor: React.FC = () => {
+const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
   const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState('# Header')
+  // const [value, setValue] = useState('# Header')
   const previewRef = useRef<HTMLDivElement | null>(null);
+
+  const { updateCell } = useActions()
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -29,7 +36,7 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={previewRef}>
-        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+        <MDEditor value={cell.content} onChange={(v) => updateCell(cell.id, v || '')} />
       </div>
     );
   }
@@ -37,7 +44,7 @@ const TextEditor: React.FC = () => {
   return (
     <div className="text-editor card" onClick={() => setEditing(true)}> 
       <div className="card-content">
-        <MDEditor.Markdown source={value} />
+        <MDEditor.Markdown source={cell.content || 'Click to edit'} />
       </div>
     </div>
   );
